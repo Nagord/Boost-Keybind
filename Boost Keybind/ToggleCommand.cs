061ba3b549b6c72d1f06fbb6e97f5b1efdb2bbab
DragonFire47 <1redyoung@gmail.com>
2021-02-17 01:27:59 -0500

    Removed Unnecesary usings

--- Boost Keybind/ToggleCommand.cs
+++ Boost Keybind/ToggleCommand.cs
@@ -1,9 +1,4 @@
 ﻿using PulsarPluginLoader.Chat.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Boost_Keybind
 {
