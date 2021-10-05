a02a1bd376bbd2fe9b1a59366dfd073d02a42d9a
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2021-10-04 22:06:54 -0400

    Updated for PML V0.10.0

---
+++ Boost Keybind/Mod.cs
@@ -0,0 +1,28 @@
+﻿using PulsarModLoader;
+
+namespace Boost_Keybind
+{
+    public class Mod : PulsarMod
+    {
+        public Mod()
+        {
+            if (bool.TryParse(PLXMLOptionsIO.Instance.CurrentOptions.GetStringValue("BoostToggleMode"), out bool result))
+            {
+                Global.togglemode = result;
+            }
+        }
+
+        public override string Version => "0.2.1";
+
+        public override string Author => "Dragon";
+
+        public override string LongDescription => "Gives Boost a keybind";
+
+        public override string Name => "Boost Keybind";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.Boost Keybind";
+        }
+    }
+}
--- Boost Keybind/ToggleCommand.cs
+++ Boost Keybind/ToggleCommand.cs
@@ -1,48 +1,31 @@
-﻿using PulsarPluginLoader.Chat.Commands;
+﻿using PulsarModLoader.Chat.Commands.CommandRouter;
 
 namespace Boost_Keybind
 {
-    class ToggleCommand : IChatCommand
+    class ToggleCommand : ChatCommand
     {
-        public string[] CommandAliases()
+        public override string[] CommandAliases()
         {
             return new string[] { "boosttogglemode", "btm" };
         }
 
-        public string Description()
+        public override string Description()
         {
             return "Enables/Disables toggle mode for boosting";
         }
 
-        public bool Execute(string arguments, int SenderID)
+        public override void Execute(string arguments)
         {
             Global.togglemode = !Global.togglemode;
             Global.CurrentToggle = false;
             PLXMLOptionsIO.Instance.CurrentOptions.SetStringValue("BoostToggleMode", Global.togglemode.ToString());
             string s = Global.togglemode ? "Enabled" : "Disabled";
-            PulsarPluginLoader.Utilities.Messaging.Notification($"Togglemode {s}");
-            return false;
-        }
-
-        public bool PublicCommand()
-        {
-            return false;
+            PulsarModLoader.Utilities.Messaging.Notification($"Togglemode {s}");
         }
 
         public string UsageExample()
         {
             return $"/{CommandAliases()[0]}";
-        }
-    }
-    [HarmonyLib.HarmonyPatch(typeof(PLServer), "Start")]
-    class LoadSetting
-    {
-        static void Postfix ()
-        {
-            if(bool.TryParse(PLXMLOptionsIO.Instance.CurrentOptions.GetStringValue("BoostToggleMode"), out bool result))
-            {
-                Global.togglemode = result;
-            }
         }
     }
 }
